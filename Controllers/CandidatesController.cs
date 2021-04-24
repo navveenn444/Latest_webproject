6050c1af2ed7c300fd6b708e15820e8cdb2d39ef
navveenn <apoli@DESKTOP-A06GMO4>
2021-04-24 00:41:58 -0400

    commmitt all

---
+++ Controllers/CandidatesController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mywebproject_core.Models;
+using static Mywebproject_core.Models.Mymodel;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Mywebproject_core.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        // GET: CandidatesController
+        HttpClient httpClient;
+
+        static string BASE_URL = "https://api.open.fec.gov/v1";
+        static string API_KEY = "ZEksIdffy3B73CsLvN2kl7YZB5nEu5mBpwP9BFC6";
+        //ZEksIdffy3B73CsLvN2kl7YZB5nEu5mBpwP9BFC6
+        public ActionResult Index()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string FEDERAL_ELECTORAL_API_PATH = BASE_URL + "/candidates?limit=20";
+            string candidatesData = "";
+
+            Candidates candidates = null;
+
+            httpClient.BaseAddress = new Uri(FEDERAL_ELECTORAL_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(FEDERAL_ELECTORAL_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    candidatesData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!candidatesData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    candidates = JsonConvert.DeserializeObject<Candidates>(candidatesData);
+                }
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(candidates);
+            //  return View();
+        }
+
+        // GET: CandidatesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CandidatesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CandidatesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CandidatesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CandidatesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CandidatesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CandidatesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Models/Mymodel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Mywebproject_core.Models.Mymodel;
+
+namespace Mywebproject_core.Models
+{
+    public class Mymodel
+    {
+        //    public class Candidatedetail
+        //    {
+
+        //        public int Candidate_ID { get; set; }
+        //        public string Candidate_Name { get; set; }
+        //        public string Candidate_State { get; set; }
+        //        public string Candidate_status {get; set;}
+        //    public ICollection<Electionyear> Electionyears { get; set; }
+        //}
+
+        //public class Electionyear
+        //{
+
+        //    public string Electionyears_ID { get; set; }
+        //    public string Election_district { get; set; }
+        //    public string Election_districtnumber { get; set; }
+        //    public virtual Candidatedetail Candidatedetail { get; set; }
+        //    public virtual Party Party { get; set; }
+        //}
+
+        //public class Party
+        //{
+        //    public int Party_ID { get; set; }
+        //    public string committee_Id { get; set; }
+        //    public string committee_type { get; set; }
+        //    public string committee_treasurername { get; set; }
+        //    public ICollection<Electionyear> Electionyears { get; set; }
+        //}
+
+        public class Candidates
+        {
+            public string api_version { get; set; }
+            public Pagination pagination { get; set; }
+            public CandidateDetail[] results { get; set; }
+        }
+
+        public class Pagination
+        {
+            public int pages { get; set; }
+            public int per_page { get; set; }
+            public int page { get; set; }
+            public int count { get; set; }
+        }
+
+        public class CandidateDetail
+        {
+            public bool has_raised_funds { get; set; }
+            public string first_file_date { get; set; }
+            public string party { get; set; }
+            public string party_full { get; set; }
+            public string last_f2_date { get; set; }
+            public string office { get; set; }
+            public int[] cycles { get; set; }
+            public object inactive_election_years { get; set; }
+            public string candidate_status { get; set; }
+            public string district { get; set; }
+            public string incumbent_challenge_full { get; set; }
+            public string name { get; set; }
+            public int active_through { get; set; }
+            public DateTime load_date { get; set; }
+            public string last_file_date { get; set; }
+            public string candidate_id { get; set; }
+            public bool candidate_inactive { get; set; }
+            public string incumbent_challenge { get; set; }
+            public string flags { get; set; }
+            public string office_full { get; set; }
+            public string[] election_districts { get; set; }
+            public bool federal_funds_flag { get; set; }
+            public int district_number { get; set; }
+            public string state { get; set; }
+            public int[] election_years { get; set; }
+
+            public Candidates[] candidateresults { get; set; }
+        }
+
+
+    }
+}
+
